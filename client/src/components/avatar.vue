921018f814dc127ec22b8f5c91e3a07590c1c64b
taytrix <128123457+taytrix@users.noreply.github.com>
2026-01-01 03:35:07 -0600

    feat: add dixscape branding and custom avatars
    - Replace logo with dixscape logo
    - Update header text to "dixscape navigator"
    - Add custom avatar images for tay, v, olive, troy
    - Modify avatar component to show custom images for specific users

--- client/src/components/avatar.vue
+++ client/src/components/avatar.vue
@@ -1,8 +1,6 @@
 <template>
-  <!--
-      <img :src="`https://ui-avatars.com/api/?name=${seed}&size=${size}`" />
-  -->
   <div
+    v-if="!hasCustomAvatar"
     class="avatar"
     :style="{
       width: size + 'px',
@@ -14,6 +12,15 @@
   >
     {{ seed.substring(0, 2).toUpperCase() }}
   </div>
+  <img
+    v-else
+    class="avatar"
+    :src="customAvatarUrl"
+    :style="{
+      width: size + 'px',
+      height: size + 'px',
+    }"
+  />
 </template>
 
 <style lang="scss" scoped>
@@ -25,11 +32,20 @@
     display: inline-block;
     overflow: hidden;
     border-radius: 50%;
+    object-fit: cover;
   }
 </style>
 
 <script lang="ts">
   import { Vue, Component, Prop } from 'vue-property-decorator'
+
+  // Custom avatars for specific users
+  const customAvatars: Record<string, string> = {
+    tay: require('@/assets/images/avatars/tay.jpg'),
+    v: require('@/assets/images/avatars/v.jpg'),
+    olive: require('@/assets/images/avatars/olive.jpg'),
+    troy: require('@/assets/images/avatars/troy.jpg'),
+  }
 
   @Component({
     name: 'neko-avatar',
@@ -37,6 +53,16 @@
   export default class extends Vue {
     @Prop(String) readonly seed: string | undefined
     @Prop(Number) readonly size: number | undefined
+
+    get hasCustomAvatar(): boolean {
+      if (!this.seed) return false
+      return this.seed.toLowerCase() in customAvatars
+    }
+
+    get customAvatarUrl(): string {
+      if (!this.seed) return ''
+      return customAvatars[this.seed.toLowerCase()] || ''
+    }
 
     Background(seed: string) {
       let a = 0,
--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <a href="https://github.com/m1k1o/neko" title="Github repository" target="_blank" class="neko">
-      <img src="@/assets/images/logo.svg" alt="n.eko" />
-      <span><b>n</b>.eko</span>
+    <a href="#" class="neko">
+      <img src="@/assets/images/logo.svg" alt="dixscape" />
+      <span><b>dix</b>scape navigator</span>
     </a>
     <ul class="menu">
       <li>
